092a07b91c24d299659c332a51e0c02b4de4b9d2
juan200220131323 <torresit2002@gmail.com>
2022-11-21 17:52:03 -0500

    tirthy commit

---
+++ proyecto 2/script.js
@@ -0,0 +1,51 @@
+let div = document.getElementById("div")
+console.log(div);
+
+let p1 = document.getElementById("p1")
+p1.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p1.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+let p2 = document.getElementById("p2")
+p2.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p2.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+let p3 = document.getElementById("p3")
+p3.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p3.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+let p4 = document.getElementById("p4")
+p4.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p4.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+let p5 = document.getElementById("p5")
+p5.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p5.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+let p6 = document.getElementById("p6")
+p6.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p6.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+
+let p7 = document.getElementById("p7")
+p7.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p7.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+
+let p8 = document.getElementById("p8")
+p8.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p8.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
+
+let p9 = document.getElementById("p9")
+p9.addEventListener("click" , function(){let respuestausuario = prompt("Escribe tu respuesta")
+p9.innerText = respuestausuario
+localStorage.setItem("respuestausuario", respuestausuario)})
+
